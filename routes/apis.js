82bfed55538d65df0c8b8098270e8896ed6d0f0c
Bob Yu-Zhen Huang <cdmyhome3@gmail.com>
2020-02-26 13:24:16 +0800

    POST /api/admin/categories

--- routes/apis.js
+++ routes/apis.js
@@ -28,6 +28,7 @@ router.delete('/admin/restaurants/:id', adminController.deleteRestaurant)
 // router.get('/admin/users', authenticatedAdmin, adminController.getUsers)
 
 router.get('/admin/categories', categoryController.getCategories)
+router.post('/admin/categories', categoryController.postCategory)
 
 // router.get('/admin/categories/:id', authenticatedAdmin, categoryController.getCategories)
 
